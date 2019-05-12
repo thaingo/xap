b7767481a7858a7060adc55fa0f4064d139e1366
ayelet <ayelet.morris@gigaspaces.com>
2019-05-12 14:21:18 +0300

    test - remove logger

--- xap-core/xap-common/src/main/java/com/gigaspaces/internal/oshi/OshiChecker.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/internal/oshi/OshiChecker.java
@@ -2,35 +2,35 @@ package com.gigaspaces.internal.oshi;
 
 import com.gigaspaces.CommonSystemProperties;
 import oshi.SystemInfo;
-import java.util.logging.Logger;
+
 
 public class OshiChecker {
 
-    private static final Logger logger = Logger.getLogger(OshiChecker.class.getName());
+//    private static final Logger logger = Logger.getLogger(OshiChecker.class.getName());
     private static final boolean enabled = initEnabled();
 
     private static final SystemInfo systemInfo = initSystemInfo();
 
     private static boolean initEnabled() {
-        logger.info("Oshi in initEnable");
+//        logger.info("Oshi in initEnable");
         String enabled = System.getProperty(CommonSystemProperties.OSHI_ENABLED, "");
 
         if(enabled.isEmpty() || Boolean.parseBoolean(enabled)){
-            logger.info("Oshi is enabled");
+//            logger.info("Oshi is enabled");
             return true;
         }
-        logger.info("Oshi in initEnable - false");
+//        logger.info("Oshi in initEnable - false");
         return false;
     }
 
     private static SystemInfo initSystemInfo() {
-        logger.info("Oshi in initSystemInfo");
+//        logger.info("Oshi in initSystemInfo");
         try {
             SystemInfo mySystemInfo = new SystemInfo();
-            logger.info("Oshi in initSystemInfo - after creating new systemInfo");
+//            logger.info("Oshi in initSystemInfo - after creating new systemInfo");
             return mySystemInfo;
         }catch (Throwable t) {
-            logger.warning("Oshi is not available "+t.getMessage());
+//            logger.warning("Oshi is not available "+t.getMessage());
         }
         return null;
     }
