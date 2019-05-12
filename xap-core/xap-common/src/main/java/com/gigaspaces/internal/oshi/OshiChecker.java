f9e9137b2b1e3df51a41b81f70c9b31f958e34c0
ayelet <ayelet.morris@gigaspaces.com>
2019-05-12 13:29:54 +0300

    add prints in Oshi checker - for debug

--- xap-core/xap-common/src/main/java/com/gigaspaces/internal/oshi/OshiChecker.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/internal/oshi/OshiChecker.java
@@ -7,23 +7,28 @@ import java.util.logging.Logger;
 public class OshiChecker {
 
     private static final Logger logger = Logger.getLogger(OshiChecker.class.getName());
-    private static final boolean enabled = false;
+    private static final boolean enabled = initEnabled();
 
     private static final SystemInfo systemInfo = initSystemInfo();
 
     private static boolean initEnabled() {
+        logger.info("Oshi in initEnable");
         String enabled = System.getProperty(CommonSystemProperties.OSHI_ENABLED, "");
 
         if(enabled.isEmpty() || Boolean.parseBoolean(enabled)){
             logger.info("Oshi is enabled");
             return true;
         }
+        logger.info("Oshi in initEnable - false");
         return false;
     }
 
     private static SystemInfo initSystemInfo() {
+        logger.info("Oshi in initSystemInfo");
         try {
-            return new SystemInfo();
+            SystemInfo mySystemInfo = new SystemInfo();
+            logger.info("Oshi in initSystemInfo - after creating new systemInfo");
+            return mySystemInfo;
         }catch (Throwable t) {
             logger.warning("Oshi is not available "+t.getMessage());
         }
