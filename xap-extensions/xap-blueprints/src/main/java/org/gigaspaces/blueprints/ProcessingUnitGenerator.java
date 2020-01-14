5c423cac5308524c99e00c6bea637972ee1c38b5
ayelet <ayelet.morris@gigaspaces.com>
2020-01-14 09:37:31 +0200

    zip the generated pu and download it - refactor

--- xap-extensions/xap-blueprints/src/main/java/org/gigaspaces/blueprints/ProcessingUnitGenerator.java
+++ xap-extensions/xap-blueprints/src/main/java/org/gigaspaces/blueprints/ProcessingUnitGenerator.java
@@ -2,7 +2,6 @@ package org.gigaspaces.blueprints;
 
 import com.gigaspaces.start.SystemLocations;
 import org.jini.rio.boot.PUZipUtils;
-
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,19 +12,20 @@ public class ProcessingUnitGenerator {
 
     private static Logger logger = Logger.getLogger(ProcessingUnitGenerator.class.getName());
 
-    public static void generate(String name, Map<String, String> properties) throws Exception {
+    public static File generate(String name, Map<String, String> properties) throws Exception {
         Blueprint blueprint = BlueprintRepository.getDefault().get(name);
 
         Path workDir = SystemLocations.singleton().work();
         Path tempDirectory = Files.createTempDirectory(workDir, "blueprint-temp");
-        Path target = blueprint.getDefaultTarget(tempDirectory);
+        Path blueprintDir = tempDirectory.resolve("blueprint");
+        Path target = blueprintDir.resolve(name);
 
         blueprint.generate(target, properties);
-        File zipFile = new File(workDir.toString()+"/"+target.getFileName()+".zip");
-        if(zipFile.exists()) {
-            zipFile.delete();
-        }
-        PUZipUtils.zip(tempDirectory.toFile(), zipFile);
+        Path zipFile = blueprintDir.resolve(target.getFileName()+".zip");
+
+        PUZipUtils.zip(blueprintDir.toFile(), zipFile.toFile());
         logger.info("************************* Target Path: "+target.toString());
+
+        return zipFile.toFile();
     }
 }
